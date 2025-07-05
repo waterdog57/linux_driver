d77fcdb1de6d3a82c8b4e8ad358316ee85b142ec
Wang Liang <liang257@gmail.com>
2025-07-05 22:22:53 +0800

    [DEV MODIFY] 1. modify cdev init and exit. If  the minor number is more than one.

--- char_device_driver/my_driver.c
+++ char_device_driver/my_driver.c
@@ -1,97 +1,112 @@
-#include <linux/init.h>
-#include <linux/uaccess.h>
-#include <linux/module.h>
-#include <linux/fs.h>
 #include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/uaccess.h>
 
 static dev_t dev;
-static struct cdev *demo_cdev;
-static signed int count = 1;
+static struct cdev *demo_cdev[2];
+static struct class *my_class;
+static signed int count = 2;
 
+static int demodrv_open(struct inode *inode, struct file *file) {
+  printk("open\n");
+  int major = MAJOR(inode->i_rdev);
+  int minor = MINOR(inode->i_rdev);
+  printk("%s, major = %d, mirno = %d\n", __func__, major, minor);
 
-static int demodrv_open(struct inode *inode, struct file *file)
-{
-	printk("open\n");
-	int major = MAJOR(inode->i_rdev);
-	int minor = MINOR(inode->i_rdev);
-	printk("%s, major = %d, mirno = %d\n",__func__,major,minor);
-
-	return 0;
+  return 0;
 }
-static int demodrv_release(struct inode *inode, struct file *file)
-{
-	return 0;
+static int demodrv_release(struct inode *inode, struct file *file) { return 0; }
+static ssize_t demodrv_read(struct file *file, char __user *buf, size_t lbuf,
+                            loff_t *ppos) {
+  printk("read\n");
+  return 0;
 }
-static ssize_t demodrv_read(struct file *file, char __user *buf, size_t lbuf, loff_t *ppos)
-{
-	printk("read\n");
-	return 0;
-}
-static ssize_t demodrv_write(struct file *file,const char __user *buf, size_t count, loff_t *f_pos)
-{
-	printk("write\n");
-	return 0;
+static ssize_t demodrv_write(struct file *file, const char __user *buf,
+                             size_t count, loff_t *f_pos) {
+  printk("write\n");
+  return 0;
 }
 
+static const struct file_operations demodrv_fops = {.owner = THIS_MODULE,
+                                                    .open = demodrv_open,
+                                                    .release = demodrv_release,
+                                                    .read = demodrv_read,
+                                                    .write = demodrv_write};
 
-static const struct file_operations demodrv_fops = {
-	.owner = THIS_MODULE,
-	.open = demodrv_open,
-	.release = demodrv_release,
-	.read = demodrv_read,
-	.write = demodrv_write
-};
+static int __init my_init(void) {
 
+  int ret;
+  int i;
+  int pass;
 
+  ret = alloc_chrdev_region(&dev, 0, count, "my_char_dev");
+  if (ret) {
+    printk("allocate fail");
+    return ret;
+  }
 
-static int __init my_init(void){
+  my_class = class_create("my_char_class");
+  for (i = 0; i < count; i++) {
+    printk("i = %d\n", i);
+    demo_cdev[i] = cdev_alloc();
+    if (!demo_cdev[i]) {
+      printk("cdev alloc fail");
+      goto unregister_chrdev;
+    }
 
-	int ret;
+    cdev_init(demo_cdev[i], &demodrv_fops);
 
-	ret = alloc_chrdev_region(&dev,0,count, "my_char_dev");
-	if( ret ){
-		printk("allocate fail");
-		return ret;
-	}
-	demo_cdev = cdev_alloc();
-	if(!demo_cdev){
-		printk("cdev alloc fail");
-		goto unregister_chrdev;
-	}
+    ret = cdev_add(demo_cdev[i], dev, i);
+    if (ret) {
+      printk("cdev add fail");
+      pass = i;
+      goto cdev_fail;
+    }
 
-	cdev_init( demo_cdev, &demodrv_fops );
-
-	ret = cdev_add(demo_cdev,dev,count);
-	if(ret){
-		printk("cdev add fail");
-		goto cdev_fail;
-	}
-	printk("major = %d, mirno = %d\n",MAJOR(dev),MINOR(dev));
-
-	return 0;
+    device_create(my_class, NULL, dev, NULL, "my_char_dev%d", i);
+  }
+  return 0;
 
 cdev_fail:
-	cdev_del(demo_cdev);
+  for (; i >= 0; i--) {
+    if (i <= pass - 1) {
+      device_destroy(my_class, MKDEV(MAJOR(dev), MINOR(dev) + i));
+    }
+    if (demo_cdev[i]) {
+
+      cdev_del(demo_cdev[i]);
+    }
+  }
 unregister_chrdev:
-	unregister_chrdev_region(dev, count);
+  unregister_chrdev_region(dev, count);
 
-	return ret;
-
+  return ret;
 }
 
-void __exit my_exit(void){
-	printk("my_exit\n");
+static void __exit my_exit(void) {
+  printk("my_exit\n");
+  int i;
+  for (i = 0; i < count; i++) {
+    if (demo_cdev[i]) {
+      cdev_del(demo_cdev[i]);
+    }
+  }
 
-	if(demo_cdev){
-		cdev_del(demo_cdev);
-	}
-	unregister_chrdev_region(dev, count);
-
+  if (my_class) {
+    int j;
+    for (j = 0; j < count; j++) {
+      device_destroy(my_class, MKDEV(MAJOR(dev), MINOR(dev) + j));
+    }
+    class_destroy(my_class);
+  }
+  unregister_chrdev_region(dev, count);
 }
 
 module_init(my_init);
 module_exit(my_exit);
-
 
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("waterdog");
